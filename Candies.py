9f2bacc87b149e94947b781156fae011bdd2fbb1
drtaylor1701 <trekker35@gmail.com>
2018-06-15 09:22:20 -0700

    updates made on 6/14

--- Candies.py
+++ Candies.py
@@ -1,7 +1,13 @@
 def candies(n, m):
-    candiesEatenPerChild = m % n
-    print(candiesEatenPerChild)
-    return candiesEatenPerChild
+    if m % n == 0:
+        candiesPerChild = m / n
+    else:
+        candiesPerChild = m / n
+        candiesPerChild = int(candiesPerChild)
+    print(candiesPerChild)
+    candiesEaten = candiesPerChild * n
+    print(candiesEaten)
+    return candiesEaten
 
 children = 3
 candy = 9
