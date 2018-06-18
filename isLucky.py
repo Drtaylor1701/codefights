1c0ef8b82235608f4f32a17e1d70fb623f707328
drtaylor1701 <trekker35@gmail.com>
2018-06-17 21:30:36 -0700

    another attempt at IsLucky - I solved it!

--- isLucky.py
+++ isLucky.py
@@ -1,21 +1,31 @@
 def isLucky(n):
-    halflengthofn == len(n)/2
-    nasstring = str(n)
-    firsthalf = nasstring[0:halflengthofn - 1]
-    secondhalf = [halflengthofn:]
-    print(firsthalf, secondhalf)
-    firsthalftotal = 0
-    secondhalftotal = 0
-    for number in firsthalf:
+    n = str(n)
+    print(n)
+    lengthOfN = len(n)
+    firsthalfIndex = int((lengthOfN / 2))
+    secondhalfIndex = int(lengthOfN / 2)
+    print(firsthalfIndex)
+    print(secondhalfIndex)
+    firstHalf = n[:firsthalfIndex]
+    print(firstHalf)
+    secondHalf = n[secondhalfIndex:]
+    print(secondHalf)
+    sumoffirsthalf = 0
+    sumofsecondhalf = 0
+    for number in firstHalf:
         number = int(number)
-        firsthalftotal == firsthalftotal + number
+        sumoffirsthalf = sumoffirsthalf + number
+        print("The first half sum is: " + str(sumoffirsthalf))
 
-    for number in secondhalf:
+    for number in secondHalf:
         number = int(number)
-        secondhalftotal = secondhalftotal + number
+        sumofsecondhalf = sumofsecondhalf + number
+        print("The second half sum is: " + str(sumofsecondhalf))
 
-    if firsthalftotal == secondhalftotal:
-        return true
+    if sumoffirsthalf == sumofsecondhalf:
+        return True
     else:
-        return false
-        
+        return False
+
+n = 1230
+isLucky(n)
