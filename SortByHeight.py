5cfab4b7e44c003581d1fdad0b23fd7c4a129964
drtaylor1701 <trekker35@gmail.com>
2018-06-19 08:19:36 -0700

    adding a sorting array to figure out how to get those numbers out of there.

---
+++ SortByHeight.py
@@ -0,0 +1,7 @@
+def sortByHeight(a):
+    sortingArray = []
+    for number in a:
+        if number != -1:
+            sortingArray.append(number)
+            print(sortingArray)
+a = [-1, 150, 190, 170, -1, -1, 160, 180]
