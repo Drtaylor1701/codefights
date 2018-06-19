1faf74d1b20be03caacc2d41783d3c08c8b9731f
drtaylor1701 <trekker35@gmail.com>
2018-06-19 08:31:06 -0700

    Python sux

--- SortByHeight.py
+++ SortByHeight.py
@@ -4,4 +4,16 @@ def sortByHeight(a):
         if number != -1:
             sortingArray.append(number)
             print(sortingArray)
+
+    sortedHeights = sorted(sortingArray)
+    print(sortedHeights)
+    aIndex = 0
+    sortedHeightsIndex = 0
+    for item in sortedHeights:
+        if a[aIndex] != -1:
+            a[aIndex] = item
+        aIndex += 1
+
+    return a
+
 a = [-1, 150, 190, 170, -1, -1, 160, 180]
