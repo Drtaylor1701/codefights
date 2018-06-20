ba600db43aafb68bc330eb3f7f11564adf5606b6
drtaylor1701 <trekker35@gmail.com>
2018-06-20 03:51:16 -0700

    finished Circle of Numbers

--- circleOfNumbers.py
+++ circleOfNumbers.py
@@ -1,9 +1,19 @@
 def circleOfNumbers(n, firstNumber):
-    if firstNumber != 0 and firstNumber > int(n * .5):
-        secondNumber = (n / 2) + firstNumber
-    elif firstNumber != 0 and firstNumber < int(n * .5):
+    if firstNumber != 0 and firstNumber > (n / 2):
         secondNumber = (n / 2) - firstNumber
+    elif firstNumber != 0 and firstNumber < (n / 2):
+        secondNumber = (n / 2) + firstNumber
+    elif firstNumber == (n / 2):
+        secondNumber = 0
     else:
         secondNumber = n / 2
 
+    if secondNumber < 0:
+        secondNumber = secondNumber * -1
+
+    print(secondNumber)
     return secondNumber
+
+n = 10
+firstNumber = 7
+circleOfNumbers(n, firstNumber)
