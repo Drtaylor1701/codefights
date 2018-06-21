13103a048bbf9428c71ceaf761ca82030be698a4
drtaylor1701 <trekker35@gmail.com>
2018-06-21 09:57:03 -0700

    Indexes getting figured out

--- SortByHeight.py
+++ SortByHeight.py
@@ -1,6 +1,7 @@
 def sortByHeight(a):
     sortingArray = []
     indexes = []
+    newArray = []
     for index in range(0, len(a)):
         if a[index] == -1:
             print(index)
@@ -12,8 +13,20 @@ def sortByHeight(a):
     for index in range(0, len(a)):
         if a[index] != -1:
             sortingArray.append(a[index])
-
+    sortingArray = sorted(sortingArray)
     print(sortingArray)
+
+    index = 0
+    sortedindex = 0
+    for index in range(0, len(a)):
+        if index in indexes:
+            a[index] = -1
+        else:
+            a[index] = sortingArray[sortedindex]
+            sortedindex += 1
+        index += 1
+        print(a)
+
 
 a = [-1, 150, 190, 170, -1, -1, 160, 180]
 sortByHeight(a)
