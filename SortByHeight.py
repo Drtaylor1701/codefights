0ee5cf5291f90b075d98b2a0f527e15a9a850b66
drtaylor1701 <trekker35@gmail.com>
2018-06-21 09:42:07 -0700

    trying to get the indexes array to actually print

--- SortByHeight.py
+++ SortByHeight.py
@@ -4,7 +4,8 @@ def sortByHeight(a):
     for index in range(0, len(a)):
         if index == -1:
             indexes.append(index)
-            print(indexes)
+
+    print(indexes)
 
 a = [-1, 150, 190, 170, -1, -1, 160, 180]
 sortByHeight(a)
