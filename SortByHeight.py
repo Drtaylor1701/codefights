cfe6d8715e04034476b31d31b81f5a633eea6f92
drtaylor1701 <trekker35@gmail.com>
2018-06-21 12:08:38 -0700

    solved sort by height

--- SortByHeight.py
+++ SortByHeight.py
@@ -4,28 +4,32 @@ def sortByHeight(a):
     newArray = []
     for index in range(0, len(a)):
         if a[index] == -1:
-            print(index)
+            print("the index is " + str(index))
             indexes.append(index)
 
-    print(indexes)
+    print("The -1s are positioned at " + str(indexes))
 
     index = 0
     for index in range(0, len(a)):
         if a[index] != -1:
             sortingArray.append(a[index])
     sortingArray = sorted(sortingArray)
-    print(sortingArray)
+    print("The sorted list of heights is " + str(sortingArray))
 
     index = 0
     sortedindex = 0
     for index in range(0, len(a)):
         if index in indexes:
             a[index] = -1
+            print("currently, the array is " + str(a))
         else:
             a[index] = sortingArray[sortedindex]
             sortedindex += 1
+            print("currently, the array is " + str(a))
         index += 1
-        print(a)
+        print(index)
+        print(sortedindex)
+    return a
 
 
 a = [-1, 150, 190, 170, -1, -1, 160, 180]
