0cc64aae4ab751cfe22965f90af4c49e699ca702
drtaylor1701 <trekker35@gmail.com>
2018-06-21 09:52:11 -0700

    trying to get the sorting array to do what I want.

--- SortByHeight.py
+++ SortByHeight.py
@@ -8,9 +8,12 @@ def sortByHeight(a):
 
     print(indexes)
 
-    while a != -1:
-        sortingArray.append(a)
-        print(sortingArray)
+    index = 0
+    for index in range(0, len(a)):
+        if a[index] != a:
+            sortingArray.append(a[index])
+
+    print(sortingArray)
 
 a = [-1, 150, 190, 170, -1, -1, 160, 180]
 sortByHeight(a)
