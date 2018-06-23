778a7a548404a320efc2872790307700c278ed47
drtaylor1701 <trekker35@gmail.com>
2018-06-23 08:11:24 -0700

    Basic logic design completed.

--- lateRide.py
+++ lateRide.py
@@ -1,9 +1,17 @@
 def lateRide(n):
-    if n % 60 == 0:
-        hours = n / 60
-        hours = str(hours)
-        print(hours)
-        lengthofHours = len(int(hours))
-        print(lengthofHours)
+    #so first I'll need the number of minutes to be divided into time more than
+    #1 hour and time less than 1 hour
+
+    #next, I'll need to separate the digits, so I'll have to convert them to strings
+
+    #then I'll need to convert each digit back to int or float and add them
+
+    #return the total
+
+    #also, this is a silly way to do anything and nobody would do this
+
+    #especially on a freaking motorcycle
+
+    
 n = 240
 lateRide(n)
