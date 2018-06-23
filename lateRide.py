6c92c284a107e86487524c206c6efddab4a51e0c
drtaylor1701 <trekker35@gmail.com>
2018-06-23 08:18:42 -0700

    This may be a solution

--- lateRide.py
+++ lateRide.py
@@ -1,17 +1,39 @@
 def lateRide(n):
-    #so first I'll need the number of minutes to be divided into time more than
-    #1 hour and time less than 1 hour
-
-    #next, I'll need to separate the digits, so I'll have to convert them to strings
-
+    #first, I'll need to convert the number to a string
+    n = str(n)
+    print("the total number of minutes is " + n)
+    #next, I'll need to separate the digits, so I'll have to convert them to
+    #strings and go through them one at a time
+    if len(n) == 4:
+        n1 = n[0]
+        n2 = n[1]
+        n3 = n[2]
+        n4 = n[3]
+    elif len(n) == 3:
+        n1 = n[0]
+        n2 = n[1]
+        n3 = n[2]
+        n4 = 0
+    elif len(n) == 2:
+        n1 = n[0]
+        n2 = n[1]
+        n3 = 0
+        n4 = 0
+    else:
+        total = n[0]
+        return total
     #then I'll need to convert each digit back to int or float and add them
-
+    n1 = int(n1)
+    n2 = int(n2)
+    n3 = int(n3)
+    n4 = int(n4)
+    total = n1 + n2 + n3 + n4
     #return the total
-
+    return total
     #also, this is a silly way to do anything and nobody would do this
 
     #especially on a freaking motorcycle
 
-    
+
 n = 240
 lateRide(n)
