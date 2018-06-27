84496dd9e52d46d841479e878eb4b8810fa9adbd
drtaylor1701 <trekker35@gmail.com>
2018-06-27 13:54:28 -0700

    another attempt at this

--- phoneCall.py
+++ phoneCall.py
@@ -1,19 +1,2 @@
 def phoneCall(min1, min2_10, min11, s):
-    longestCall = 0
-    if s > min1:
-        longestCall = 1
-        s = s - min1
-    else:
-        print(s, longestCall)
-        return longestCall
-
-    for i in range(2, 11):
-        if s > min2 * i:
-            print(s, longestCall)
-        longestCall = longestCall + i
-        s = s - (min2 * i)
-        print(s, longestCall)
-        return longestCall
-
-    print(s, longestCall)
-    return longestCall
+    print(min1, min2_10, min11, s)
