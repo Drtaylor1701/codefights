63d40af02ffb9e491da00aef57ce04b23fb42c7c
drtaylor1701 <trekker35@gmail.com>
2018-06-28 07:25:50 -0700

    set code for first minute

--- phoneCall.py
+++ phoneCall.py
@@ -1,5 +1,13 @@
 def phoneCall(min1, min2_10, min11, s):
+    minutes = 0
     print(min1, min2_10, min11, s)
+    while s > min11:
+        while s > min2_10:
+            while s > min1 and minutes < 1:
+                minutes += 1
+                s = s - min1
+                print(minutes, s)
+
 
 min1 = 3
 min2_10 = 1
