6b41f7af75c69128675592cbf366f5d47e04318f
drtaylor1701 <trekker35@gmail.com>
2018-06-28 07:29:09 -0700

    added code for minute 11

--- phoneCall.py
+++ phoneCall.py
@@ -2,7 +2,7 @@ def phoneCall(min1, min2_10, min11, s):
     minutes = 0
     print(min1, min2_10, min11, s)
     while s > min11:
-        while s > min2_10 and minutes <= 10:
+        while s > min2_10 and minutes < 10:
             while s > min1 and minutes < 1:
                 minutes += 1
                 s = s - min1
@@ -10,6 +10,10 @@ def phoneCall(min1, min2_10, min11, s):
             minutes += 1
             s = s - min2_10
             print(minutes, s)
+        minutes += 1
+        s = s - min11
+        print(minutes, s)
+    return minutes
 
 
 min1 = 3
