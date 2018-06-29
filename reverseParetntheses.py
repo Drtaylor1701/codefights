133e9db7b183ce3531603c8976c0003d569ec3fc
drtaylor1701 <trekker35@gmail.com>
2018-06-28 18:10:12 -0700

    changes to reverse parentheses problem

--- reverseParetntheses.py
+++ reverseParetntheses.py
@@ -1,11 +1,23 @@
 def reverseParentheses(s):
+    openParentheses = 0
     index = 0
-    while index < len(s):
-        if s[index] == "(" or s[index] == ")":
-            print(s[index], index)
-            index += 1
-        else:
-            print("there are no parentheses")
+    output = ""
+    while openParentheses == 0:
+        if s[index] == "(":
+            print(index)
+            openParentheses = 1
+        index += 1
+        print(index)
+
+    while openParentheses == 1:
+        if s[index] != "(" and s[index] != ")" and index <= len(s):
+            print(s[index])
+            output = output + s[index]
+            print(output)
+        elif s[index] == ")" and index <= len(s):
+            if ")" not in s[index:]:
+                openParentheses = 0
+        index += 1
 
 s = "a(bc)de"
 reverseParentheses(s)
