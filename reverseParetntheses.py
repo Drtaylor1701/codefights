1e262f2a22b103eb15ca3e178676c7c13fef134b
drtaylor1701 <trekker35@gmail.com>
2018-06-28 18:09:53 -0700

    Trying to get it to stop being an endless loop

--- reverseParetntheses.py
+++ reverseParetntheses.py
@@ -2,7 +2,10 @@ def reverseParentheses(s):
     index = 0
     while index < len(s):
         if s[index] == "(" or s[index] == ")":
-            print(s[index])
+            print(s[index], index)
+            index += 1
+        else:
+            print("there are no parentheses")
 
 s = "a(bc)de"
 reverseParentheses(s)
