37aad1c5faa277f8978fcca7946aaf5ba67caa75
drtaylor1701 <trekker35@gmail.com>
2018-07-01 08:48:50 -0700

    while loops not running

--- phoneCall.py
+++ phoneCall.py
@@ -1,24 +1,29 @@
 def phoneCall(min1, min2_10, min11, s):
     minutes = 0
-    print(min1, min2_10, min11, s)
-    while s >= min11 and s >= 0:
-        while s >= min2_10 and minutes <= 10:
-            while s >= min1 and minutes <= 1:
-                minutes += 1
-                s = s - min1
-                print(minutes, s)
+    while minutes < 1:
+        if s >= 0:
+            s = s - min1
             minutes += 1
+            print(s, minutes)
+
+    while minutes < 2 and minutes < 11:
+        if s >= 0:
             s = s - min2_10
-            print(minutes, s)
-        minutes += 1
-        s = s - min11
-        print(minutes, s)
+            minutes += 1
+            print(s, minutes)
+
+    while minutes >= 10:
+        if s >= 0:
+            s = s - min11
+            minutes += 1
+            print(s, minutes)
+
+    print(minutes)
     return minutes
 
-
-min1 = 2
-min2_10 = 2
-min11 = 1
-s = 2
+min1 = 3
+min2_10 = 1
+min11 = 2
+s = 20
 
 phoneCall(min1, min2_10, min11, s)
