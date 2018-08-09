1c9fc1a90631767255e2eae84663dc3721503f50
drtaylor1701 <trekker35@gmail.com>
2018-08-09 12:41:32 -0700

    solved knapsack lite

--- knapsackLite.py
+++ knapsackLite.py
@@ -6,6 +6,10 @@ def knapsackLight(value1, weight1, value2, weight2, maxW):
             totalValue = 0
         elif weight1 > weight2:
             totalValue = value1
+        elif weight1 == maxW and value1 > value2:
+            totalValue = value1
+        elif weight2 == maxW and value2 > value1:
+            totalValue = value2
         else:
             totalValue = value2
 
