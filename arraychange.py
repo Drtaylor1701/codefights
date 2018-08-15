90305cb11e0de25b3b31a9fcfb7ed02a5e12ce57
drtaylor1701 <trekker35@gmail.com>
2018-08-15 15:48:43 -0700

    Used https://github.com/dvitsios/codesignal-my-solutions/blob/master/arrayChange/arrayChange.py

--- arraychange.py
+++ arraychange.py
@@ -1,8 +1,10 @@
 def arrayChange(inputArray):
-    index = 0
+    index = -1
+    changecount = 0
     for item in inputArray:
-        if index != 0:
-            if item != inputArray[index - 1]:
-                item += 1
-    index += 1
-    return inputArray
+        if index == -1:
+            index += 1
+        while item != (inputArray[index] + 1):
+            inputArray[index + 1] = item + 1
+            changecount += 1
+    return changecount
